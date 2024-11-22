21e1bf60d47b8fa214d44b44df8b082c516751d6
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-11-22 10:09:00 +0700

    Membuat fungsi Rencana Study View beserta dengan parameternya

--- app/src/main/java/com/example/praktikumnavigasi/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikumnavigasi/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,11 @@
 package com.example.praktikumnavigasi.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import com.example.praktikumnavigasi.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){}
