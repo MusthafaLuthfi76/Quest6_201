19d35e62e78a0f7f30216a4753bf172d1f061f2a
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-11-22 09:49:02 +0700

    Tambah Import untuk menghilangkan error di dropdown

--- app/src/main/java/com/example/praktikumnavigasi/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/praktikumnavigasi/ui/widget/DynamicSelectedField.kt
@@ -1,6 +1,7 @@
 package com.example.praktikumnavigasi.ui.widget
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
@@ -13,6 +14,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicSelectedTextField(
     selectedValue: String,
