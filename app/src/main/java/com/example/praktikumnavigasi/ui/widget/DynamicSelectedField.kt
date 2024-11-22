5a82a69ffae0ffca2297f56166328f87e172001f
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-11-22 09:30:15 +0700

    Menambahkan parameter di fungsi DynamicSelectedTextField

--- app/src/main/java/com/example/praktikumnavigasi/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/praktikumnavigasi/ui/widget/DynamicSelectedField.kt
@@ -1,6 +1,13 @@
 package com.example.praktikumnavigasi.ui.widget
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun DynamicSelectedTextField(){}
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier= Modifier
+){}
